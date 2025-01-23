b7fd4a214e53f95bb553ae4f234376f20f9e6f73
lingbend <lingbend@gmail.com>
2025-01-22 22:11:31 -0700

    Fixed blank body crash. Formatted printing of links better

--- CanvasScraper.py
+++ CanvasScraper.py
@@ -32,16 +32,20 @@ def main():
             print('error')
         else:
             new_page_list.append(i.url) # adding the unique part of the URL to a list
-        time.sleep(1)
+        # time.sleep(1)
     important_pages = []
 
     for j in new_page_list: # printing the url of each page
         print(j)
         html_page = course.get_page(j).body
-        if (mode == "-h"):
+        if (html_page is None):
+            continue
+        elif (mode == "-h"):
             h5p_mode(html_page, important_pages, j, root, course_num)
 
-    print(important_pages)
+    print("\nPages matching criteria:\n")
+    for k in important_pages:
+        print(k)
 
 def h5p_mode(page_body, important_pages, j, root, course_num):
     if '.h5p.com' in page_body or (course_num + '/external_tools/retrieve?') in page_body or 'h5p-iframe' in page_body:
